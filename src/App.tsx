9903e895b65697f0466b5e52b57be285682e6967
Rishabh Mahto <itsrishabh13@gmail.com>
2026-01-29 03:12:37 +0530

    Mini's Initialized

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import GridReverse from './components/grid-reverse'
+
+function App() {
+
+  return (
+    <>
+      <GridReverse />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/grid-reverse.tsx
@@ -0,0 +1,12 @@
+export default function GridReverse() {
+  return (
+    <div className="flex items-center justify-center h-screen w-screen">
+      <div>
+        <h1>Grid Reverse</h1>
+        <div className="flex gap-4">
+          <input type="number" placeholder="Enter the number of Cells..." />
+        </div>
+      </div>
+    </div>
+  );
+}
